a9d1ba4385636dae8e31d8772a0ff1c1fd73f3e8
Savvanis Spyros <savvaniss@yahoo.gr>
2016-11-27 15:00:33 +0200

    previues commit

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class Actor extends Model
 {
+    protected $table='actors';
     protected $fillable = [
         'first_name',
         'last_name',
