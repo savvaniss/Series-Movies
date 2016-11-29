00459469b3036c6a0a33e885a35eac18815f8889
malfuncti0n <savvaniss@yahoo.gr>
2016-11-29 14:33:13 +0200

    last commit

--- app/Controllers/SeriesController.php
+++ app/Controllers/SeriesController.php
@@ -5,36 +5,34 @@ namespace App\Controllers;
 use App\Models\Actor;
 use App\Models\Series;
 use App\Models\Tag;
+use App\Models\Image;
 use Slim\Views\Twig as View;
 use Respect\Validation\Validator as v;
 use Cocur\Slugify\Slugify as slug;
 use Carbon\Carbon as carbon;
+use App\Services\DataApi;
+
 
 class SeriesController extends Controller
 {
     protected $datadir='datahouse';
+    
+    
     public function getCreateSeries($request,$response)
     {
         return $this->container->view->render($response, 'series.create.twig');
     }
 
     public function postCreateSeries($request,$response){
-
-//        $image=$request->getUploadedFiles();
-//        var_dump($image);
-//        echo "error";
-//        var_dump($image['input-file-preview']->getError());
-//        echo "mediatype";
-//        var_dump($image['input-file-preview']->getClientMediaType());
-//        echo "filename";
-//        var_dump($image['input-file-preview']->getClientFilename());
-//        echo "size";
-//        var_dump($image['input-file-preview']->getSize());
-//        echo "stream";
-//        var_dump($image['input-file-preview']->getStream());
-//        //moveTo();
-//        die();
-
+        
+       //get files
+       $image=$request->getUploadedFiles();
+       //if image has error do not proceed
+       if($image['input-file-preview']->getError()){
+           $this->flash->addMessage('error', 'your image faled to upload');
+            return $response->withRedirect($this->router->pathFor('series.create'));
+       }
+       
         //validate input
         $validation = $this->validator->validate($request, [
             // ->emailAvailable()
@@ -44,6 +42,7 @@ class SeriesController extends Controller
             'release_day'=>v::date()
         ]);
         if ($validation->failed()) {
+            $this->flash->addMessage('error', 'Your series was unable to create');
             return $response->withRedirect($this->router->pathFor('series.create'));
         }
         //use slug class to sluglify series name
@@ -58,7 +57,7 @@ class SeriesController extends Controller
             'release_day' => $date,
             'slug' => $sluglified
         ]);
-
+            
         //explode tags in array split by comma
         $tags=explode(',',$request->getParam('tag'));
         foreach($tags as $tag){
@@ -85,6 +84,37 @@ class SeriesController extends Controller
 
         }
         
+       //calculate hash and get image data
+       $dataApi=new DataApi;
+       $imageFilename=$image['input-file-preview']->getClientFilename();
+       $imageSize=$image['input-file-preview']->getSize();
+       $imageHash=$dataApi->calculateHash($imageFilename);
+       
+       //check if file exist in datadir
+       if(!$dataApi->hashExistInData($imageHash)){
+          //if not exist den we move it in upload dir 
+           var_dump( move_uploaded_file($image['input-file-preview']->file,__DIR__ . '/../'.$this->datadir.'/'.$imageHash)) ;
+          //and we make a new entry in database
+           $image=new Image;
+           $image->filename=$imageFilename;
+           $image->hash=$imageHash;
+           $image->slug=$slug->slugify($image->name);
+           $image->size=$imageSize;
+           $image->uploaded_dir=__DIR__ . '/../'.$this->datadir.'/'.$imageHash;
+       }else{
+           //if file exist with the same hash then our file exist from onother upload
+           //find it in database and make new entry
+           $imageExist=Image::where('hash','=',$imageHash)->first();
+           $image=new Image;
+           $image->filename=$imageExist->filename;
+           $image->hash=$imageExist->hash;
+           $image->slug=$imageExist->slug;
+           $image->size=$imageExist->size;
+           $image->uploaded_dir=$imageExist->uploaded_dir;
+       }
+       //save image in database
+       $series->images()->save($image);
+   
         $this->flash->addMessage('success', 'Your Series created!');
         return $response->withRedirect($this->router->pathFor('series.show'));
 
